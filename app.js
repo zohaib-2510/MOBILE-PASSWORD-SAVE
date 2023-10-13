4b7b3454e50104e20d053ff15d43541cad19d7d8
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-10-13 12:21:07 -0700

    complete

---
+++ app.js
@@ -0,0 +1,10 @@
+const inp = document.getElementById("inp").value;
+const form = document.getElementById("form").value;
+const inp2 = document.getElementById("s2").value;
+form.addEventListener("submit",function(){
+    const pass = inp.value;
+    if(pass===inp2){
+        console.log("jjjhvh");
+    }
+})
+
